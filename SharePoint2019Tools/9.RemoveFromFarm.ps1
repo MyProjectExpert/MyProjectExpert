842efd99f503490754669b889c85e3d53af4a46a
Michael Wharton <mwharton@whartoncomputer.com>
2019-01-10 14:37:04 -0500

    Added Script to remove SharePoint farm

---
+++ SharePoint2019Tools/9.RemoveFromFarm.ps1
@@ -0,0 +1,13 @@
+﻿<#
+  Remove SharePoint Farm
+  Run on SharePoint Server Node
+    
+#>
+
+Set-ExecutionPolicy "Unrestricted" -ErrorAction SilentlyContinue -Confirm:$false -Verbose
+Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+
+Remove-SPConfigurationDatabase
+
+# get-help Remove-SPConfigurationDatabase -Examples
+
