d06d6c97254e1f3df3f5406faa65d1cb446d67d7
Michael Wharton <mwharton@whartoncomputer.com>
2019-01-10 14:16:27 -0500

    Added 9.RemoveFromFarm.ps1 scrpt
    Remove SharePoint Farm and then rebuild using scripts

---
+++ Azure-SharePoint-IaaS/9.RemoveFromFarm.ps1
@@ -0,0 +1,8 @@
+﻿
+Set-ExecutionPolicy "Unrestricted" -ErrorAction SilentlyContinue -Confirm:$false -Verbose
+Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+
+Remove-SPConfigurationDatabase
+
+# get-help Remove-SPConfigurationDatabase -Examples
+
