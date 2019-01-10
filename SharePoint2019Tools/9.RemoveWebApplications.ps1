ec1264e233d4ea40897a077863b3b0971f654572
Michael Wharton <mwharton@whartoncomputer.com>
2019-01-10 14:37:59 -0500

    Added script to remove web application

---
+++ SharePoint2019Tools/9.RemoveWebApplications.ps1
@@ -0,0 +1,16 @@
+﻿<#
+  Remove Web Applications in SharePoint Farm
+  and delete the IIS and database   
+#>
+
+Set-ExecutionPolicy "Unrestricted" -ErrorAction SilentlyContinue -Confirm:$false -Verbose
+Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+
+Function Remove-Web ($WebAppName){
+Write-Host -ForegroundColor Yellow "Remove Web Application: " $WebAppName
+Remove-SPWebApplication -Identity $WebAppName -RemoveContentDatabases:$true -DeleteIISSite:$true -Confirm:$false -Verbose
+}
+
+#######################################################
+$WebAppName     = "DemoSP"
+Remove-Web $WebAppName
