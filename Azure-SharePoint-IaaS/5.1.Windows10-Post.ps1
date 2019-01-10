b088cb95d8911ba7056e811c469a08632775e762
Michael Wharton <mwharton@whartoncomputer.com>
2019-01-10 13:44:04 -0500

    update 5.1.Windows10-Post.ps1

--- Azure-SharePoint-IaaS/5.1.Windows10-Post.ps1
+++ Azure-SharePoint-IaaS/5.1.Windows10-Post.ps1
@@ -2,12 +2,11 @@
 Use for SharePoint Saturday Demo.
 SharePoint Server 2019 - Single Server Farm
 
-
 Architect
-DEMOAD - Active Directory must be running
-DEMOSQL   - SQL configure as below
-DEMOSP    - Build
-DEMOWIN10 - Windows 10 client
+    DEMOAD    - Active Directory must be running
+    DEMOSQL   - SQL Server
+    DEMOSP    - SharePoint Server
+    DEMOWIN10 - Windows 10 client
 
 Post Activities
 1) Update 
@@ -16,6 +15,4 @@ Post Activities
    remove VP6
    Join Domain
 3 Reboot
-=======
-
 #>
