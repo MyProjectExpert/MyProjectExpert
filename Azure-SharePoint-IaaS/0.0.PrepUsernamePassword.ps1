d791b7457dc9356bc72389a5ae1e3affad2c5812
Michael Wharton <mwharton@whartoncomputer.com>
2019-01-15 00:57:10 -0500

    Feature for license key

--- Azure-SharePoint-IaaS/0.0.PrepUsernamePassword.ps1
+++ Azure-SharePoint-IaaS/0.0.PrepUsernamePassword.ps1
@@ -12,7 +12,7 @@
         Dont update and store back to Github
 #>
 
-function Create-CredFile{
+function Add-CredFile{
     Param([string]$un, [string]$pw, [string]$credFN)
     $secpass  = $pw |ConvertTo-SecureString -AsPlainText -Force  
     $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $un, $secPass  
@@ -20,6 +20,12 @@ function Create-CredFile{
     write-host "Create Password File: " $credFN " for account: " $un  -ForegroundColor Yellow
 }
 
+function Add-LicenseFile{
+    Param([string]$licenseKey, [string]$licenseFN)
+    $secKey  = $licenseKey |ConvertTo-SecureString -AsPlainText -Force  
+    $secKey | Export-Clixml -Path $licenseFN  
+    write-host "Create License Key File: " $licenseKey  -ForegroundColor Yellow
+}
 # Create directory if not found
 $path = "C:\safe"
 If(!(test-path $path))
@@ -31,25 +37,41 @@ If(!(test-path $path))
 $userName = "acctName"
 $passWord = "passWord"
 $credFileName = "C:\safe\local-acctName.txt"
-Create-CredFile $userName $passWord $credFileName
+Add-CredFile $userName $passWord $credFileName
 
 # domain\mawharton
 $userName = "farmadmin"
 $passWord = "passWord"
 $credFileName = "C:\safe\domainName-farmadmin.txt"
-Create-CredFile $userName $passWord $credFileName
+Add-CredFile $userName $passWord $credFileName
 
 # domain\farmadmin
 $userName = "ServiceSP"
 $passWord = "passWord"
 $credFileName = "C:\safe\domainName-ServiceSP.txt"
-Create-CredFile $userName $passWord $credFileName
+Add-CredFile $userName $passWord $credFileName
 
 # domain\SericeSQL
 $userName = "ServiceSQL"
 $passWord = "passWord"
 $credFileName = "C:\safe\domainName-ServiceSQL.txt"
-Create-CredFile $userName $passWord $credFileName
+Add-CredFile $userName $passWord $credFileName
 
+# SharePoint Server License Key
+$licenseKey = "aaaa-bbbb-ccccc-ddddd-eeeee-ffff"
+$licenseFileName = "C:\safe\SharePoint-LicenseKey.txt"
+Add-LicenseFile $licenseKey $licenseFileName
 
+# Project Server License Key
+$licenseKey = "aaaa-bbbb-ccccc-ddddd-eeeee-ffff"
+$licenseFileName = "C:\safe\ProjectServer-LicenseKey.txt"
+Add-LicenseFile $licenseKey $licenseFileName
+
+# example to unencrypt credentials
+# $credServiceSP        = Import-CliXml -Path 'C:\safe\domain-ServiceSP.txt’ 
+
+# example to unencrypt license keys
+# $secureKey = Import-CliXml -Path "C:\safe\SharePoint-License.txt"
+# $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureKey)
+# $SharePointLicenseKey = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
